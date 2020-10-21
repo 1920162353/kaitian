a562fe40b56e2698931245bdbf41f43f156aa81e
gyx <1920162353@qq.com>
2020-10-21 11:24:51 +0800

    123

--- gg/eureka-grade/src/main/java/com/example/eurekagrade/controller/StudentController.java
+++ gg/eureka-grade/src/main/java/com/example/eurekagrade/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aaa")
+@RequestMapping("/aa")
 public class StudentController {
 
     @Autowired
